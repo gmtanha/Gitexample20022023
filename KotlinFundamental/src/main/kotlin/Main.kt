921fcc5aeddd19f17bdd498dd74c273c9c989870
Ha <ha@example.com>
2023-02-27 18:06:07 +0700

    kieu du lieu

---
+++ KotlinFundamental/src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+//     dấu // là comment
+//    1: khai báo biến
+//    mutable, immutable
+//    Từ báo khai biến là var
+
+//    var name = "Phạm Tấn Phát"
+//    name = "Phạm Tấn Phát 1"
+//    print(name)
+    // Từ khóa khai báo val
+    val pi = 3.14
+    val address = "199 Lý Thường Kiệt"
+    // 2: Kiểu dữ liệu
+    // kiểu String: Chuổi
+    var text: String = "Hello"
+    // kiểu sồ nguyên: Int, Long
+    var age: Int = 29
+    // Kiểu số thực: Float
+    var weight: Float = 74.9f
+
+
+ }
