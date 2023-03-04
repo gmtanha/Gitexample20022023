6991e4c0ff3896e14a8795f23a87632980fbbdbb
Ha <ha@example.com>
2023-03-04 22:50:07 +0700

    kieu du lieu

--- Kotlinfundamental2/src/main/kotlin/Main.kt
+++ Kotlinfundamental2/src/main/kotlin/Main.kt
@@ -4,5 +4,13 @@ fun main(args: Array<String>) {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
-    print("Hi")
+    var month = 6
+    var message = when (month){
+        in 1..3 -> "Quý 1"
+        in 4..6 -> "Quý 2"
+        in 7..9 -> "Quý 3"
+        in 10..12 -> "Quý 4"
+        else -> "Không thuộc quý nào"
+    }
+    println(message)
 }
